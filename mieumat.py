5a513cfcbb0f1a5066fb923b741cdf4fdd00bcda
Lee-jae-yoon <jyl3859@naver.com>
2017-05-22 17:05:55 +0900

    Add files via upload

---
+++ mieumat.py
@@ -0,0 +1,31 @@
+﻿import turtle
+wn = turtle.Screen()
+t1 = turtle.Turtle()
+
+def giyukat(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    t1.fd(size)
+    t1.right(90)
+    t1.fd(size)
+    
+def nieunat(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    t1.fd(size)
+    t1.left(90)
+    t1.fd(size)
+
+def mieumat(x,y,size):
+    giyukat(x,y,size)
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    t1.setheading(270)
+    nieunat(x,y,size)
+
+mieumat(100,100,100)
+
+wn.exitonclick()
